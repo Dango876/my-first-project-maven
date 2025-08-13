ed088abe21d04ec4736ed99ab7f63436ccafb43d
Artem Vasin <dangelos876@gmail.com>
2025-08-13 16:35:08 +0300

    make the following changes

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("hello world");
         // реализуйте алгоритм здесь
     }
 }
