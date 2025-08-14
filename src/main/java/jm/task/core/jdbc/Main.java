cd3ce3a958973ecea81a0e1ae196e9e93dde4773
Artem Vasin <dangelos876@gmail.com>
2025-08-14 19:34:39 +0300

    HotFix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,8 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Алекс", "Смит", (byte) 82);
+        userService.saveUser("Алексис", "Смит", (byte) 74);
+        userService.saveUser("Джон", "Рекорд", (byte) 60);
+        userService.saveUser("Ник", "Прогон", (byte) 75);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
